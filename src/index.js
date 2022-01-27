37adbf33ac2d6f36b97d86c141bf19fae2494a78
ziomararj <ziomara.jimenez@gmail.com>
2022-01-26 17:51:44 -0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-import validator from './validator.js';
+//Import the object validator from the validator.js file
+import validator from "./validator.js";
 
-console.log(validator);
+//Select input
+const creditCardNumber = document.getElementById("input");
+let verify = document.getElementById("verify");
+
+// function verificar() {
+//   console.log(
+//     validator.isValid(creditCardNumber.value),
+//     validator.maskify(creditCardNumber.value)
+//   );
+//   let resIsValid = validator.isValid(creditCardNumber.value);
+//   let resMaskify = validator.maskify(creditCardNumber.value);
+
+//   creditCardNumber.value = resMaskify;
+//   if (resIsValid) {
+//     document.getElementById("valido").style.display = "block";
+//     document.getElementById("input").style.borderColor = "green";
+//     document.getElementById("invalido").style.display = "none";
+//   } else {
+//     document.getElementById("valido").style.display = "none";
+//     document.getElementById("input").style.borderColor = "red";
+//     document.getElementById("invalido").style.display = "block";
+//   }
+// }
+
+//Add event listener
+verify.addEventListener("click", function () {
+  console.log(
+    validator.isValid(creditCardNumber.value),
+    validator.maskify(creditCardNumber.value)
+  );
+  let resIsValid = validator.isValid(creditCardNumber.value);
+  let resMaskify = validator.maskify(creditCardNumber.value);
+
+  creditCardNumber.value = resMaskify;
+  console.log(resIsValid, resMaskify);
+  if (resIsValid) {
+    document.getElementById("valido").style.display = "block";
+    document.getElementById("input").style.borderColor = "green";
+    document.getElementById("invalido").style.display = "none";
+  } else {
+    document.getElementById("valido").style.display = "none";
+    document.getElementById("input").style.borderColor = "red";
+    document.getElementById("invalido").style.display = "block";
+  }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,84 @@
+// Create an object with the functions is Valid and Maskify
 const validator = {
-  // ...
+  isValid: (creditCardNumber) => {
+    let data = creditCardNumber.split("").map(Number);
+    console.log(data);
+
+    let imp = [];
+    for (let i = 0; i < data.length; i++) {
+      if (i % 2 === 0) {
+        imp.push(data[i]);
+      }
+    }
+    console.log(imp);
+
+    let multiplicado = [];
+    function mult() {
+      for (let i = 0; i < imp.length; i++) {
+        multiplicado[i] = imp[i] * 2;
+      }
+    }
+    mult();
+
+    let mayores = [];
+    let menores = 0;
+    let iguales = 0;
+    let arreglo = [];
+    let arregloSuma = 0;
+
+    for (let i = 0; i < multiplicado.length; i++) {
+      if (multiplicado[i] > 10) {
+        mayores += multiplicado[i];
+      } else if (multiplicado[i] < 10) {
+        menores += multiplicado[i];
+      } else if (multiplicado[i] === 10) {
+        iguales++;
+      }
+    }
+
+    arreglo = mayores.split("").map(Number);
+    for (let i = 0; i < arreglo.length; i++) {
+      arregloSuma += arreglo[i];
+    }
+
+    let sumado = 0;
+    function suma() {
+      for (let i = 0; i < data.length; i++) {
+        sumado += data[i];
+      }
+    }
+    suma();
+
+    let sumado1 = 0;
+    function suma1() {
+      for (let i = 0; i < imp.length; i++) {
+        sumado1 += imp[i];
+      }
+    }
+    suma1();
+
+    let diferencia = sumado - sumado1;
+    let sumaTotal = diferencia + arregloSuma + menores + iguales;
+
+    return sumaTotal % 10 === 0 ? true : false;
+  },
+
+  maskify: (creditCardNumber) => {
+    // const number1 = creditCardNumber.slice(creditCardNumber.length - 4);
+    // // const number2 = creditCardNumber.slice(0, creditCardNumber.length - 4);
+
+    // let maskify1 = `############${number1}`;
+    // return maskify1;
+    let newMaskify = "";
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i < creditCardNumber.length - 4) {
+        newMaskify += "#";
+      } else {
+        newMaskify += creditCardNumber[i];
+      }
+    }
+    return newMaskify;
+  },
 };
 
 export default validator;
